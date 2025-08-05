8a9937af789c5eb843165473d9d248da6a576cbd
king_cyrus <adeyemiemma45@gmail.com>
2025-08-05 05:09:24 +0100

    server3 commit

--- server/src/config/redis.ts
+++ server/src/config/redis.ts
@@ -14,7 +14,7 @@ export const initRedis = async (): Promise<RedisClientType> => {
     password:process.env.REDIS_PASSWORD,
     socket: {
         host:process.env.REDIS_HOST,
-        port: Number(process.env.REDIS_PORT),
+        port:  10964,
     }
 });
 
